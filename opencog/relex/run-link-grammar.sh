727839f1169fea908c37ccef60c6a62929de56fd
Vitaly Bogdanov <vsbogd@gmail.com>
2019-07-23 20:16:11 +0300

    Replace opencog by singnet and singularitynet

--- opencog/relex/run-link-grammar.sh
+++ opencog/relex/run-link-grammar.sh
@@ -4,6 +4,6 @@
 #
 ./stop.sh relex-link-grammar
 docker run --rm --name="relex-link-grammar" -p 9000:9000 \
-   -w /home/Downloads/relex-master opencog/relex /bin/sh link-grammar-server.sh
+   -w /home/Downloads/relex-master singularitynet/relex /bin/sh link-grammar-server.sh
 
 clear
--- opencog/relex/run-plain.sh
+++ opencog/relex/run-plain.sh
@@ -4,6 +4,6 @@
 #
 ./stop.sh relex-plain
 docker run --rm --name="relex-plain" -p 3333:3333 \
-   -w /home/Downloads/relex-master opencog/relex /bin/sh plain-text-server.sh
+   -w /home/Downloads/relex-master singularitynet/relex /bin/sh plain-text-server.sh
 
 clear
